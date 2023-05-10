1e50d31b94ccb3ae326077348467bc876da5e749
RicardoSua <ricardo.su.dv@gmail.com>
2023-05-10 16:27:16 +0100

    Download image and check

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from download_image import download_image
+
 if __name__ == '__main__':
-    print('Hello World!')
+    image_url = 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
+
+    img_bytes = download_image(image_url)
+
+    print(img_bytes)
